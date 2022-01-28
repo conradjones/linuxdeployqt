e2f63a589404985210a8e6ba2d729dfc735cd0c9
Conrad Jones <jonesconrad1@gmail.com>
2022-01-28 22:14:27 +0000

    Update shared.cpp

--- tools/linuxdeployqt/shared.cpp
+++ tools/linuxdeployqt/shared.cpp
@@ -1126,7 +1126,7 @@ DeploymentInfo deployQtLibraries(const QString &appDirPath, const QStringList &a
        }
 
        LogNormal() << "Using qmake: " << qmakePath;
-       QString output = captureOutput(qmakePath , {" -query"});
+       QString output = captureOutput(qmakePath , {"-query"});
        LogDebug() << "-query output from qmake:" << output;
 
        QStringList outputLines = output.split("\n", Qt::SkipEmptyParts);
